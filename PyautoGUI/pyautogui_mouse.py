d973d755cb2c27cfdf8fd0ec041980962698da1e
gopinathan-1806 <gopinath180695@gmail.com>
2026-08-26 15:17:44 +0530

    Create pyautogui_mouse.py

---
+++ PyautoGUI/pyautogui_mouse.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+# pyautogui.scroll(-500)
+# time.sleep(1)
+# pyautogui.scroll(500)
+
+# pyautogui.rightClick(100, 100)
+# pyautogui.moveTo(200, 200, duration=1)
+# pyautogui.doubleClick(200, 200)
+# pyautogui.leftClick(200, 200)
+pyautogui.screenshot('screenshot.png')
